256565fc526068ca03138dec027ec431d5539d09
Karan812001 <85431238+Karan812001@users.noreply.github.com>
2021-06-26 19:18:11 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import App from "./App";
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright by ShapeAI @ {new Date().getFullYear()}</p>
+    </footer>
+  );
+}
+export default Footer;
